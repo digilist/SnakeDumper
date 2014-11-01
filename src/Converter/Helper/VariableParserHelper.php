c5b83bf7dc82c1925cbf01a71591ced1db7625a4
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2014-11-01 22:55:02 +0100

    Added Variable Parser

---
+++ src/Converter/Helper/VariableParserHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Digilist\SnakeDumper\Converter\Helper;
+
+use Digilist\SnakeDumper\Exception\VariableParserException;
+
+/**
+ * The VariableParserHelper helps parsing variable expression strings.
+ */
+class VariableParserHelper
+{
+
+    const LEFT_DEFAULT = '{{';
+    const RIGHT_DEFAULT = '}}';
+
+    /**
+     * @param string $str
+     *      Subjected string containing variable expressions.
+     * @param array $replacements
+     *      Dictionary with replacement values, like {'search' => 'replacement'}.
+     * @param string $left
+     *      The left delimiter for a variable expression. Default is `{{`.
+     * @param string $right
+     *      The right delimiter for a variable expression. Default is `}}`.
+     * @return string
+     * @throws \Digilist\SnakeDumper\Exception\VariableParserException
+     */
+    public static function parse($str, array $replacements, $left = self::LEFT_DEFAULT, $right = self::RIGHT_DEFAULT)
+    {
+        $left = preg_quote($left);
+        $right = preg_quote($right);
+
+        $regex = '/' . $left . '\\s*(\\w+)\\s*' . $right . '/';
+
+        $handler = function (array $matches) use ($replacements) {
+            $key = $matches[1];
+            if (isset($replacements[$key])) {
+                return $replacements[$key];
+            }
+
+            throw new VariableParserException('No value defined for `' . $key . '`.');
+        };
+
+        return preg_replace_callback($regex, $handler, $str);
+    }
+}
---
+++ src/Exception/VariableParserException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Digilist\SnakeDumper\Exception;
+
+/**
+ * @package Digilist\SnakeDumper\Exception
+ * @author moellers
+ */
+class VariableParserException extends \RuntimeException
+{
+}
